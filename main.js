71e66dec4c598a2cb67264e9249232e8a42f241b
Vlada FKZ Laptop <vlada030@gmail.com>
2021-11-05 14:51:28 +0100

    postavljen normal view sa styled controls

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const videoContainer = document.querySelector("#videoContainer");
 const video = document.querySelector("#video");
 const videoControls = document.querySelector("#videoControls");
 const playpause = document.getElementById("playpause");
+const rewind = document.getElementById("rewind");
+const forward = document.getElementById("forward");
 const stop = document.getElementById("stop");
 const mute = document.getElementById("mute");
 const volinc = document.getElementById("volinc");
@@ -11,6 +13,13 @@ const voldec = document.getElementById("voldec");
 const progress = document.getElementById("progress");
 const progressBar = document.getElementById("progress-bar"); // za browsere koji ne prikazu progress tag
 const fullscreen = document.getElementById("fs");
+
+const svgPlay = document.getElementById("svgPlay");
+const svgPause = document.getElementById("svgPause");
+const svgMute = document.getElementById("svgMute");
+const svgUnmute = document.getElementById("svgUnmute");
+const svgExitFS = document.getElementById("svgExitFS");
+const svgEnterFS = document.getElementById("svgEnterFS");
 
 // proveri da li browser podrzava video tag i u zavisnosti od toga omoguci ili disable custom controls
 const isVideoSupported = !!document.createElement("video").canPlayType;
@@ -26,8 +35,12 @@ if (isVideoSupported) {
 playpause.addEventListener("click", () => {
     if (video.paused || video.ended) {
         video.play();
+        svgPlay.classList.toggle("disabled", true);
+        svgPause.classList.toggle("disabled", false);
     } else {
         video.pause();
+        svgPlay.classList.toggle("disabled", false);
+        svgPause.classList.toggle("disabled", true);
     }
 });
 
@@ -35,18 +48,43 @@ stop.addEventListener("click", () => {
     video.pause();
     video.currentTime = 0;
     progress.value = 0;
+    svgPlay.classList.toggle("disabled", false);
+    svgPause.classList.toggle("disabled", true);
 });
 
 mute.addEventListener("click", () => {
     video.muted = !video.muted;
+    svgMute.classList.toggle("disabled");
+    svgUnmute.classList.toggle("disabled");
+    // if (video.muted) {
+    //     svgMute.classList.toggle('disabled', false)
+    //     svgUnmute.classList.toggle('disabled', true)
+    // } else {
+    //     svgMute.classList.toggle('disabled', true)
+    //     svgUnmute.classList.toggle('disabled', false)
+    // }
 });
 
 volinc.addEventListener("click", () => {
+    if (video.muted) return;
+    console.log(video.volume);
     alterVolume("+");
+
+    if (video.volume > 0.1 && !video.muted) {
+        svgMute.classList.toggle("disabled", true);
+        svgUnmute.classList.toggle("disabled", false);
+    }
 });
 
 voldec.addEventListener("click", () => {
+    if (video.muted) return;
+    console.log(video.volume);
     alterVolume("-");
+
+    if (video.volume < 0.1) {
+        svgMute.classList.toggle("disabled", false);
+        svgUnmute.classList.toggle("disabled", true);
+    }
 });
 
 const alterVolume = (dir) => {
@@ -80,11 +118,34 @@ video.addEventListener("timeupdate", () => {
 // klik na progress bar za seeking
 progress.addEventListener("click", function (e) {
     var rect = this.getBoundingClientRect();
-    console.log(rect);
     var pos = (e.pageX - rect.left) / rect.width;
     video.currentTime = pos * video.duration;
     //progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100) + "%";
 });
+
+rewind.addEventListener("click", () => {
+    alterTime("-");
+});
+
+forward.addEventListener("click", () => {
+    alterTime("+");
+});
+
+const alterTime = (dir) => {
+    let time;
+
+    if (dir === "-") {
+        time = video.currentTime < 5 ? 0 : video.currentTime - 5;
+    } else if (dir === "+") {
+        time =
+            video.currentTime + 5 > video.duration
+                ? video.duration
+                : video.currentTime + 5;
+    }
+
+    video.currentTime = time;
+    progress.setAttribute("value", video.currentTime);
+};
 
 // FULLSCREEN MODE
 
@@ -104,6 +165,17 @@ if (!isFullScreenEnabled) {
 
 fullscreen.addEventListener("click", () => {
     handleFullscreen();
+    svgEnterFS.classList.toggle("disabled", videoContainer.dataset.fullscreen);
+    svgExitFS.classList.toggle("disabled", !videoContainer.dataset.fullscreen);
+    console.log(videoContainer.dataset.fullscreen);
+
+    // if (videoContainer.dataset.fullscreen) {
+    //     svgEnterFS.classList.toggle('disabled', true)
+    //     svgExitFS.classList.toggle('disabled', false)
+    // } else {
+    //     svgEnterFS.classList.toggle('disabled', false)
+    //     svgExitFS.classList.toggle('disabled', true)
+    // };
 });
 
 const handleFullscreen = () => {
@@ -137,20 +209,19 @@ const isFullScreenActive = () => {
     );
 };
 // pomocna fja koja video kontejneru dodaje data atribut fullscreen / boolean da bi detektovali mod i lakse se stilizovali elementi u odgovarajucem modu
-var setFullscreenData = function(state) {
-    videoContainer.setAttribute('data-fullscreen', !!state);
- }
+var setFullscreenData = function (state) {
+    videoContainer.setAttribute("data-fullscreen", !!state);
+};
 
- document.addEventListener('fullscreenchange', function(e) {
+document.addEventListener("fullscreenchange", function (e) {
     setFullscreenData(!!(document.fullscreen || document.fullscreenElement));
- });
- document.addEventListener('webkitfullscreenchange', function() {
+});
+document.addEventListener("webkitfullscreenchange", function () {
     setFullscreenData(!!document.webkitIsFullScreen);
- });
- document.addEventListener('mozfullscreenchange', function() {
+});
+document.addEventListener("mozfullscreenchange", function () {
     setFullscreenData(!!document.mozFullScreen);
- });
- document.addEventListener('msfullscreenchange', function() {
+});
+document.addEventListener("msfullscreenchange", function () {
     setFullscreenData(!!document.msFullscreenElement);
- });
-
+});
